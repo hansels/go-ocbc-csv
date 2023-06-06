e24a3ab9c35e3eff4e628de80a8ff43000cab772
Hansel Susanto <hansel@susanto.me>
2023-06-06 11:03:37 +0700

    push csv

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
@@ -28,25 +29,32 @@ func main() {
 		fmt.Printf("File Size: %+v\n", handler.Size)
 		fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-		tempFile, err := ioutil.TempFile("temp-images", handler.Filename)
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer tempFile.Close()
-
 		fileBytes, err := ioutil.ReadAll(file)
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		tempFile.Write(fileBytes)
+		if _, err := os.Stat("csv"); os.IsNotExist(err) {
+			os.Mkdir("csv", 0755)
+		}
+
+		fo, err := os.Create("csv/" + handler.Filename)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer fo.Close()
+
+		_, err = fo.Write(fileBytes)
+		if err != nil {
+			fmt.Println(err)
+		}
 
 		fmt.Fprintf(w, "Successfully Uploaded File\n")
 	})
 
 	r.Get("/csv", func(w http.ResponseWriter, r *http.Request) {
 		filename := r.URL.Query().Get("fileName")
-		tempFile, err := ioutil.ReadFile("temp-images/" + filename + ".csv")
+		tempFile, err := ioutil.ReadFile("csv/" + filename + ".csv")
 		if err != nil {
 			fmt.Println(err)
 		}
